f7568a4a887d2d3b96ab2a5d7c093fcbe8d6b3b9
Yulia Bandura <yuliabandura@gmail.com>
2021-06-06 19:26:18 +0300

    add file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # goit-node-hw-01
+
+Посилання на картинку з виконаним дз:
+https://monosnap.com/file/tlfbg5xEmRdPqaHAffRMq0sS3ch5DL
---
+++ index.js
@@ -0,0 +1,37 @@
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+// console.log(listContacts());
+// console.log(getContactById(4));
+// console.log(removeContact(5));
+// console.log(addContact("Mango", "mango@gmail.com", "033-456-56"));
+
+const argv = require("yargs").argv;
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
+
+    case "get":
+      getContactById(id);
+      break;
+
+    case "add":
+      addContact(name, email, phone);
+      break;
+
+    case "remove":
+      removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
